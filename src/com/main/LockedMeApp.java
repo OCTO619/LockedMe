9dc38f770bc0b1dd193fa1ab02eaee77d6d52ba6
OCTO619 <ishi2004singh@gmail.com>
2025-06-22 01:14:21 +0530

    project structure and main file completed

---
+++ src/com/main/LockedMeApp.java
@@ -0,0 +1,46 @@
+package com.main;
+
+import java.util.Scanner;
+
+import com.service.FileManager;
+import com.service.SubMenu;
+
+public class LockedMeApp {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		FileManager fm=new FileManager();
+		SubMenu sm=new SubMenu();
+		System.out.println("  Welcome to LockedMe.com");
+        System.out.println("  Developer: Ishita Singh");
+        String con="";
+        int choice;
+        do {
+            System.out.println("\nMain Menu:\n1:View Files in Ascending Order\n2: File Operations\n3: Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();  
+
+            switch (choice) {
+                case 1:
+                	fm.listFiles();
+                    break;
+                case 2:
+                		sm.showFileOperations(sc);
+                    break;
+
+                case 3:
+                	System.out.println("Thank you! Exiting LockedMe.com...");
+                    sc.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        System.out.println("do you want to continue(y/n)");
+		con = sc.next();
+	}while(con.equals("y"));
+	System.out.println("Thank you!");
+        sc.close();
+	}
+
+}
---
+++ src/com/service/SubMenu.java
@@ -0,0 +1,9 @@
+package com.service;
+
+import java.util.Scanner;
+
+public class SubMenu {
+	public void showFileOperations(Scanner sc) {
+		
+	}
+}
